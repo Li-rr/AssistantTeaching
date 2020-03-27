cddfa738ca0d6b224fb63dbf336dfbf2ac563d86
lrr <921090271@qq.com>
2020-03-27 22:14:28 +0800

    使用python manage.py inspectdb，反向生成模型代码

--- usermanage/models.py
+++ usermanage/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Stu(models.Model):
+    stuno = models.CharField(db_column='stuNo', primary_key=True, max_length=8)  # Field name made lowercase.
+    stuname = models.CharField(db_column='stuName', max_length=255, blank=True, null=True)  # Field name made lowercase.
+    sex = models.CharField(max_length=2, blank=True, null=True)
+    class_field = models.CharField(db_column='class', max_length=6, blank=True, null=True)  # Field renamed because it was a Python reserved word.
+
+    class Meta:
+        managed = False
+        db_table = 'stu'
+
