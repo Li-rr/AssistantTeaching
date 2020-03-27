e85245f05b8d6660f4b7aadf3d4f7e426dab0a64
lrr <921090271@qq.com>
2020-03-27 23:03:55 +0800

    完成查看学生名单功能

--- usermanage/views.py
+++ usermanage/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from usermanage.models import Stu
 # Create your views here.
 
 def index(request):
     return render(request,"index.html")
 
+def getStuTable(request):
+    all_stu = Stu.objects.all()
+    print(type(all_stu),len(all_stu))
+    print(all_stu[0],type(all_stu[0]))
+    return render(request,'stuTable.html',{'all_stu':all_stu})
 
