3bbb027bd715f9b155145b8572000c03e623edf6
lrr <921090271@qq.com>
2020-03-27 23:29:40 +0800

    添加指向作业管理与考勤管理的链接

--- usermanage/urls.py
+++ usermanage/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('',views.index,name="index"),
     path('stuTable/',views.getStuTable,name="getTable"),
+    path('attn/',views.attendanceManage,name="attnManage"),
+    path('homework/',views.homeworkManage,name="homeworkManage"),
 ]
