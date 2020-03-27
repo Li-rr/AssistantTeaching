7935775c059f038c91e4ead3e197eda7303b7676
lrr <921090271@qq.com>
2020-03-28 01:33:03 +0800

    添加了考勤管理与作业管理功能，还有实现

--- usermanage/views.py
+++ usermanage/views.py
@@ -6,9 +6,20 @@ from usermanage.models import Stu
 def index(request):
     return render(request,"index.html")
 
+# 返回学生名单
 def getStuTable(request):
     all_stu = Stu.objects.all()
     print(type(all_stu),len(all_stu))
     print(all_stu[0],type(all_stu[0]))
     return render(request,'stuTable.html',{'all_stu':all_stu})
+
+# 考勤管理
+def attendanceManage(request):
+    return HttpResponse("这里是考勤管理")
+
+# 作业管理
+def homeworkManage(request):
+    return HttpResponse("这里是作业管理")
+
+
 
